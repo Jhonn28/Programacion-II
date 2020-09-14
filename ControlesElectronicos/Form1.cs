f066a0d2eff5e61fcbf90d1ae5f13e9eed2f13ff
José Arizo <jmarizo95@hotmail.com>
2020-09-14 11:48:40 -0500

    Modi

--- ControlesElectronicos/Form1.cs
+++ ControlesElectronicos/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-//Freddy Alverca
+
 namespace ControlesElectronicos
 {
     public partial class MundoCelular : Form
